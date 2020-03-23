3c81f13263152442531973d464c8f1eac692e94e
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-23 12:13:08 +0100

    First test for Customer Tests.

---
+++ test/Acme.BLTest/CustomerTest.cs
@@ -0,0 +1,27 @@
+using System;
+using Acme.BL;
+using Xunit;
+
+namespace Acme.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Balbo",
+                LastName = "Biggins"
+            };
+            var expected = "Balbo Biggins";
+
+            //Act
+            var actual = customer.FullName;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
