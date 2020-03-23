74a93a1e5f037734e4d8d5eeb1b20d3ecd954b4a
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-23 13:12:53 +0100

    Added tests for customer full name.

--- test/Acme.BLTest/CustomerTest.cs
+++ test/Acme.BLTest/CustomerTest.cs
@@ -23,5 +23,77 @@ namespace Acme.BLTest
             //Assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void FullNameWithEmptyFirstNameTest()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = String.Empty,
+                LastName = "Biggins"
+            };
+            var expected = "Biggins";
+
+            //Act
+            var actual = customer.FullName;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameWithEmptyLastNameTest()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Balbo",
+                LastName = string.Empty
+            };
+            var expected = "Balbo";
+
+            //Act
+            var actual = customer.FullName;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameWithNullAsFirstNameTest()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = null,
+                LastName = "Biggins"
+            };
+            var expected = "Biggins";
+
+            //Act
+            var actual = customer.FullName;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameWithNullAsLastNameTest()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Balbo",
+                LastName = null
+            };
+            var expected = "Balbo";
+
+            //Act
+            var actual = customer.FullName;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
