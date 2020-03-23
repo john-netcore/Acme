fc8ee102a633db1c758ff1bc3bd88efb0692c7ea
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-23 11:55:26 +0100

    Business Logic Project and Test project first commit.

---
+++ src/Acme.BL/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Acme.BL
+{
+    public class Customer
+    {
+        public int CustomerID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+    }
+}
