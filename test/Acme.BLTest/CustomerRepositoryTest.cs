546efcf4a02ef5bb37aff321a0d099b7f1487563
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-24 13:58:44 +0100

    Implemented tests for CustomerRepository class.

---
+++ test/Acme.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+using Acme.BL;
+
+namespace Acme.BLTest
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public void RetrieveExistingCustomerTest()
+        {
+            //Given
+            var repository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                EmailAddress = "baggins@hotmail.com"
+            };
+            //When
+            var actual = repository.Retrieve(1);
+            //Then
+            Assert.Equal(expected.CustomerID, actual.CustomerID);
+            Assert.Equal(expected.FullName, actual.FullName);
+            Assert.Equal(expected.EmailAddress, actual.EmailAddress);
+        }
+
+        [Fact]
+        public void SaveValidCustomerTest()
+        {
+            //Given
+            var repository = new CustomerRepository();
+            var customer = new Customer(2)
+            {
+                LastName = "Lundgren",
+                EmailAddress = "lundgren@gmail.com"
+            };
+            //When
+            bool isSaved = repository.Save(customer);
+            //Then
+            Assert.True(isSaved);
+        }
+    }
+}
