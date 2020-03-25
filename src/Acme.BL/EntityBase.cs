00d6763729e84323399f6cf40dfa981a694198bf
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-25 11:39:01 +0100

    Created EntityBase class for entities.

---
+++ src/Acme.BL/EntityBase.cs
@@ -0,0 +1,16 @@
+namespace Acme.BL
+{
+    public enum EntityStateOption
+    {
+        Active,
+        Deleted
+    }
+
+    public abstract class EntityBase
+    {
+        public EntityStateOption EntityState { get; set; }
+        public bool HasChanges { get; set; }
+        public bool IsNew { get; private set; }
+        public bool IsValid { get { return true; } }
+    }
+}
