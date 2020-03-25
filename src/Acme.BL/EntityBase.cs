fd62347451474af318eb947a5e02d47c94536d32
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-25 17:07:07 +0100

    Added abstract method declaration and property.

--- src/Acme.BL/EntityBase.cs
+++ src/Acme.BL/EntityBase.cs
@@ -11,6 +11,8 @@ namespace Acme.BL
         public EntityStateOption EntityState { get; set; }
         public bool HasChanges { get; set; }
         public bool IsNew { get; private set; }
-        public bool IsValid { get { return true; } }
+        public bool IsValid => Validate();
+
+        public abstract bool Validate();
     }
 }
