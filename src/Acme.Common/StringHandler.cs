85cf0d1199f72eaa42a45e5e80a16df1c28247f6
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-26 05:03:04 +0100

    Added project reference to solution file.

--- src/Acme.Common/StringHandler.cs
+++ src/Acme.Common/StringHandler.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿using System.Text;
 
 namespace Acme.Common
 {
     public static class StringHandler
     {
+        public static string InsertSpaces(string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                return source;
+            }
+            var result = new StringBuilder(string.Empty);
 
+            foreach (var letter in source)
+            {
+                if (char.IsUpper(letter) && result.ToString() != string.Empty)
+                {
+                    result.Append(" ");
+                }
+                result.Append(letter);
+            }
+
+            return result.ToString();
+        }
     }
 }
