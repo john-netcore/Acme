b05a0d619565b54d8e1c5d4f9c139728dc59c3bd
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-26 05:58:21 +0100

    Implemented tests for InsertSpaces method.

--- test/Acme.CommonTest/StringHandlerTest.cs
+++ test/Acme.CommonTest/StringHandlerTest.cs
@@ -1,4 +1,4 @@
-using System;
+using Acme.Common;
 using Xunit;
 
 namespace Acme.CommonTest
@@ -6,9 +6,62 @@ namespace Acme.CommonTest
     public class StringHandlerTest
     {
         [Fact]
-        public void Test1()
+        public void InsertSpacesWithValidStringTest()
         {
+            //Arrange
+            string input = "GiantMouseTrap";
+            string expected = "Giant Mouse Trap";
+            //Act
+            string actual = StringHandler.InsertSpaces(input);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
 
+        [Fact]
+        public void InsertSpacesWithNullValueTest()
+        {
+            //Arrange
+            string input = null;
+            //Act
+            string actual = StringHandler.InsertSpaces(input);
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void InsertSpacesWithEmptyStringTest()
+        {
+            //Arrange
+            string input = string.Empty;
+            string expected = string.Empty;
+            //Act
+            string actual = StringHandler.InsertSpaces(input);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertSpacesWithAlreadyExistingSpacesTest()
+        {
+            //Arrange
+            string input = "Giant Mouse Trap";
+            string expected = "Giant Mouse Trap";
+            //Act
+            string actual = StringHandler.InsertSpaces(input);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertSpacesWithAlreadyExistingSpacesWithLowerCaseTest()
+        {
+            //Arrange
+            string input = "Giant mouse trap";
+            string expected = "Giant mouse trap";
+            //Act
+            string actual = StringHandler.InsertSpaces(input);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
